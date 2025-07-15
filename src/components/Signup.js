c8f06cf8c7bc4e3067287d5cc4850e5824a06d58
Alfi Noushad <Alfinoushad@gmail.com>
2025-07-15 23:26:52 +0530

    signup page setup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -33,7 +33,33 @@ const Signup = ({ setToken }) => {
 
 
   return (
-   s
+    <div className='container p-4'>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        login(username, password);
+      }}>
+        <div className="text-2xl pb-4">SIGN UP.. .</div>
+        <div className="mb-4">
+          <label htmlFor="exampleInputEmail1" className="form-label">Username</label>
+          <input type="text" className="form-control" id="exampleInputEmail1" value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <div id="emailHelp" className="form-text">We'll never share your username with anyone else.</div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          {authLoading ? "Signing up..." : "Submit"}
+        </button>
+      </form>
+      <div className="text my-3">Have an account already?
+        <Link to="/login"><span className='mx-3 text-blue-700 hover:underline font-semibold cursor-pointer'>Login</span></Link>
+      </div>
+    </div>
   )
 }
 
